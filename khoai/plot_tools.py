2c93f0bd8f9c3fad63283ef9f4239e80a9d6f4e6
Le Huynh Duc <lhduc94@gmail.com>
2020-01-08 11:30:33 +0700

    update

--- khoai/plot_tools.py
+++ khoai/plot_tools.py
@@ -1,20 +1,21 @@
 import matplotlib.pyplot as plt
 from sklearn.metrics import auc, roc_curve
 
+
 def plot_roc_curve(y_true,y_pred_prob):
     """ A function plot Roc AUC.
     Parameters:
-        		y_true: Array
-					True label
-        		y_pred_prob: Array
-					Probability predicted label
-	Output:
-        		roc_auc
+                y_true: Array
+                    True label
+                y_pred_prob: Array
+                    Probability predicted label
+    Output:
+                roc_auc
     """
-    plt.figure(figsize=(17,10))
+    plt.figure(figsize=(17, 10))
     fpr, tpr, thresholds = roc_curve(y_true, y_pred_prob)
-    roc_auc = auc(fpr, tpr) # compute area under the curve
-    plt.figure(figsize=(17,10))
+    roc_auc = auc(fpr, tpr)  # compute area under the curve
+    plt.figure(figsize=(17, 10))
     plt.plot(fpr, tpr, label='ROC curve (area = %0.5f)' % (roc_auc))
     plt.plot([0, 1], [0, 1], 'k--')
     plt.xlim([0.0, 1.0])
@@ -24,10 +25,11 @@ def plot_roc_curve(y_true,y_pred_prob):
     plt.title('Receiver operating characteristic')
     plt.legend(loc="lower right")
     ax2 = plt.gca().twinx()
-    ax2.plot(fpr, thresholds, markeredgecolor='r',linestyle='dashed', color='r')
-    ax2.set_ylabel('Threshold',color='r')
-    ax2.set_ylim([thresholds[-1],thresholds[0]])
-    ax2.set_xlim([fpr[0],fpr[-1]])
+    ax2.plot(fpr, thresholds, markeredgecolor='r',
+             linestyle='dashed', color='r')
+    ax2.set_ylabel('Threshold', color='r')
+    ax2.set_ylim([thresholds[-1], thresholds[0]])
+    ax2.set_xlim([fpr[0], fpr[-1]])
     plt.show()
 
-    return roc_auc 
+    return roc_auc
