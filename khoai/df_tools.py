27cb67686a975dca7659d33490674f0a651139c3
Le Huynh Duc <lhduc94@gmail.com>
2020-01-08 13:57:45 +0700

    Update docstring

--- khoai/df_tools.py
+++ khoai/df_tools.py
@@ -59,7 +59,21 @@ def reduce_mem_usage(df, verbose=True):
 
 
 def calibrate(data, train_pop, target_pop, sampled_train_pop, sampled_target_pop):
-    """
+    """ Calibrate data after undersample data.
+    Parameters:
+                data: float array
+                    probability prediction Array.
+                train_pop: int
+                    Number of data in training data.
+                target_pop: int
+                    Number of target class (minority class) in the training dataset.
+                sampled_train_pop: int
+                    Number of data in training dataset after undersampling.
+                sampled_target_pop: int
+                    Number of the target class(minority class) in the training dataset after undersampling.
+    Return:     
+                data: float array
+                    probability prediction Array after calibrate.
     """
     A = data * (target_pop / train_pop) / (sampled_target_pop / sampled_train_pop)
     B = (1 - data) * (1 - target_pop / train_pop) / (1 - sampled_target_pop / sampled_train_pop)
