ee76f04aa299e8ee730a9cadd42ca10a88d9830b
Le Huynh Duc <lhduc94@gmail.com>
2020-12-15 18:03:29 +0700

    add plot corr

--- khoai/plotly.py
+++ khoai/plotly.py
@@ -2,7 +2,9 @@
 """Plot Tools."""
 import matplotlib.pyplot as plt
 from sklearn.metrics import auc, roc_curve
+import seaborn as sns
 import numpy as np
+import pandas as pd
 
 
 def plot_roc_curve(y_true, y_pred_prob, show_threshold=False):
@@ -83,3 +85,76 @@ def plot_multi_roc_curve(y_trues, y_pred_probs, labels):
     plt.show()
 
     return fig, roc_aucs
+
+
+def heatmap(x, y, size, color, **params):
+    n_colors = params.get('n_colors', 256)  # Use 256 colors for the diverging color palette
+    palette = sns.diverging_palette(20, 220, n=n_colors)  # Create the palette
+    color_min, color_max = [-1, 1]
+
+    def value_to_color(val):
+        val_position = float((val - color_min)) / (color_max - color_min)
+        ind = int(val_position * (n_colors - 1))  # target index in the color palette
+        return palette[ind]
+
+    plt.figure(figsize=params.get('figsize', None))
+    plot_grid = plt.GridSpec(1, 15, hspace=0.2, wspace=0.1)  # Setup a 1x15 grid
+    ax = plt.subplot(plot_grid[:, :-1])
+
+    # Mapping from column names to integer coordinates
+    x_labels = [v for v in sorted(x.unique())]
+    y_labels = [v for v in sorted(y.unique())]
+    x_to_num = {p[1]: p[0] for p in enumerate(x_labels)}
+    y_to_num = {p[1]: p[0] for p in enumerate(y_labels)}
+
+    ax.scatter(
+        x=x.map(x_to_num),
+        y=y.map(y_to_num),
+        s=size * params.get('size_scale', 500),  # Vector of square sizes, proportional to size parameter
+        c=color.map(value_to_color),
+        marker=params.get('marker', 's')  # Use square as scatterplot marker
+    )
+
+    # Show column labels on the axes
+    ax.set_xticks([x_to_num[v] for v in x_labels])
+    ax.set_xticklabels(x_labels, rotation=params.get('x_rotation', 45), horizontalalignment='right')
+    ax.set_yticks([y_to_num[v] for v in y_labels])
+    ax.set_yticklabels(y_labels)
+
+    # bar plot
+    ax = plt.subplot(plot_grid[:, -1])  # Use the rightmost column of the plot
+
+    col_x = [0] * len(palette)  # Fixed x coordinate for the bars
+    bar_y = np.linspace(color_min, color_max, n_colors)  # y coordinates for each of the n_colors bars
+
+    bar_height = bar_y[1] - bar_y[0]
+    ax.barh(
+        y=bar_y,
+        width=[5] * len(palette),  # Make bars 5 units wide
+        left=col_x,  # Make bars start at 0
+        height=bar_height,
+        color=palette,
+        linewidth=0
+    )
+    ax.set_xlim(1, 2)  # Bars are going from 0 to 5, so lets crop the plot somewhere in the middle
+    ax.grid(False)  # Hide grid
+    ax.set_facecolor('white')  # Make background white
+    ax.set_xticks([])  # Remove horizontal ticks
+    ax.set_yticks(np.linspace(min(bar_y), max(bar_y), 3))  # Show vertical ticks for min, middle and max
+    ax.yaxis.tick_right()
+
+
+def plot_corr(df, columns=None, **params):
+    if columns is None:
+        columns = df.columns
+    corr = df[columns].corr()
+    corr = pd.melt(corr.reset_index(),
+                   id_vars='index')  # Unpivot the dataframe, so we can get pair of arrays for x and y
+    corr.columns = ['x', 'y', 'value']
+    heatmap(
+        x=corr['x'],
+        y=corr['y'],
+        size=corr['value'].abs(),
+        color=corr['value'],
+        **params
+    )
