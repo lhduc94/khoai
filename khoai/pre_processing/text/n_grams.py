4326395081fbe99d6b52c01e78356ca6f7d5b180
Le Huynh Duc <lhduc94@gmail.com>
2021-01-05 18:24:40 +0700

    add determiner

--- khoai/pre_processing/text/n_grams.py
+++ khoai/pre_processing/text/n_grams.py
@@ -1,9 +1,9 @@
 
-def create_ngrams(tokens, ngram=2):
+def create_ngrams(tokens, ngram=2, determiner='_'):
     """Create n_grams"""
     new_tokens = []
     for i in range(len(tokens)-ngram+1):
-        new_token = '_'.join(tokens[i:i+ngram])
+        new_token = determiner.join(tokens[i:i+ngram])
         new_tokens.append(new_token)
     return new_tokens
 
